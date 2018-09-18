649752899f79e863f992971492ab883656964f78
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-09-18 13:12:42 +0200

    added beam and get user

--- lib/pluggy/models/user.ex
+++ lib/pluggy/models/user.ex
@@ -4,6 +4,10 @@ defmodule Pluggy.User do
 
 	alias Pluggy.User
 
+	def get do
+		Postgrex.query!(DB, "SELECT id, mail, username FROM users", [], pool: DBConnection.Poolboy)
+	end
+
 	def get(id) do
 		Postgrex.query!(DB, "SELECT id, username FROM users WHERE id = $1 LIMIT 1", [id],
         pool: DBConnection.Poolboy
