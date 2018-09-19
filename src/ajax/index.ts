22ce1b5972abd80942a234e73b7181423a43b93a
zackotterstrom <zack.otterstrom@elev.ga.ntig.se>
2018-09-19 10:35:42 +0200

    Made Typescript work with our ajax

--- src/ajax/index.ts
+++ src/ajax/index.ts
@@ -1,5 +1,5 @@
-export function get(url : string) {
-    return new Promise((resolve, reject) => {
+export function get(url : string) : Promise<User>{
+    return new Promise((resolve : (resp : User) => void, reject) => {
         const request = new XMLHttpRequest()
         request.open("GET", url, true)
         request.send()
@@ -7,14 +7,20 @@ export function get(url : string) {
             if(request.readyState == XMLHttpRequest.DONE) {
                 if(request.getResponseHeader("content-type") == "application/json; charset=utf-8") 
                 {
-                    let resp : object = JSON.parse(request.responseText)
+                    let resp = JSON.parse(request.responseText)
                     resolve(resp)
                 }
                 else 
-                {
+                { 
                     reject(new Error("That's not an json"))
                 }
             }
         }
     })
 }
+
+export interface User {
+    username: string;
+    id: number;
+}
+  
